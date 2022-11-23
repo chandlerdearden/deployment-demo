b89c55b58db6a8571bee0c6230c4af9547daa813
Chandler Dearden <chandlerjdearden@gmail.com>
2022-11-23 14:57:54 -0700

    Macy added

--- public/main.js
+++ public/main.js
@@ -3,3 +3,16 @@ const btn = document.querySelector('button')
 const clickHandler = () => alert('The cat name is: Rascal')
 
 btn.addEventListener('click', clickHandler)
+
+const btn2 = document.querySelector('#button2')
+
+const getCat = () => {
+    axios.get('http://localhost:4000/api/cat')
+    .then(res => {
+        alert(res.data)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+btn2.addEventListener('click', getCat)
